405d270a338545774e9090021294c92037d6cea0
saffordkatya <katya@Chirons-MacBook-Air-2.local>
2017-07-25 21:00:43 -0500

    added Header, Recipes and ShoppingList components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Test Description', 'https://ichef.bbci.co.uk/news/660/cpsprodpb/1325A/production/_88762487_junk_food.jpg'),
+    new Recipe('Test Recipe2', 'Test Description2', 'https://ichef.bbci.co.uk/news/660/cpsprodpb/1325A/production/_88762487_junk_food.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
