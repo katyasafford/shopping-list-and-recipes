6d96ea79cf12513f8297e2823aeb2fe607686e04
saffordkatya <katya@Chirons-MacBook-Air-2.local>
2017-08-07 16:06:56 -0500

    added ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,30 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Hot Dog',
+      'This is a sad hot dog recipe',
+      'http://masarica.cl/images/hotdog2.jpg',
+      [
+        new Ingredient('Sausage', 1),
+        new Ingredient('Hotdog bun', 1)
+      ]
+    ),
+    new Recipe(
+      'Burger',
+      'This is a sad burger recipe',
+      'http://aht.seriouseats.com/images/20110228-in-n-out-secret-menu%20-%2004.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
 
   getRecipes() {
