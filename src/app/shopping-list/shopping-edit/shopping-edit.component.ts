17562d48be2a4dd0b019400cf3195834f50d16f6
saffordkatya <katya@Chirons-MacBook-Air-2.local>
2017-08-21 16:34:42 -0500

    added Clear and Delete buttons

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -57,4 +57,14 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
+  onClear() {
+    this.shoppingListForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.onClear();
+  }
+
 }
