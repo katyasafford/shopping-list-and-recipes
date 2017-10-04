c2fa0b37441669723850e6643d80f4934f429f1b
Katya Safford <katya.safford@digital-nebula.com>
2017-10-04 07:53:14 -0500

    added firebase auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    //initialize firebase when app starts
+    firebase.initializeApp({
+      apiKey: "AIzaSyAF8wXvsLLyqwVyf_EJcprB6Phe9PbAxT0",
+      authDomain: "ng-recipe-book-6c119.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
